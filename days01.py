e4c7f34dc0eb37c1847092b04513853dcdd2cda6
webdoli <topofweird@gmail.com>
2025-09-11 13:37:29 +0900

    first

---
+++ days01.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])
+print( a )
+print( a.shape )
+print( a.dtype )
+print( a.size )
+print( a.ndim )
+print( a.itemsize )
+print( a.nbytes )
+print( a.strides )
+print( a.flags )
+print( a.real )
